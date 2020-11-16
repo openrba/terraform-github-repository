4a270f54898294fe7695896c5a9303bc7e603e22
Joshua Hiscocks <joshua.hiscocks@lexisnexisrisk.com>
2020-11-16 10:51:40 +0000

    Admin team added to GitHub

--- main.tf
+++ main.tf
@@ -47,3 +47,30 @@ resource "github_team_repository" "team_write" {
   repository = github_repository.workspaces[each.key].name
   permission = "push"
 }
+
+# Administration Team
+resource "github_team" "admin" {
+  name        = "tfe-prod-admins"
+  description = "Terraform Enterprise Production Administrators"
+  privacy     = "secret"
+}
+
+resource "github_team_membership" "hiscjo01" {
+  team_id  = github_team.admin.id
+  username = "hiscjo01"
+  role     = "member"
+}
+
+resource "github_team_membership" "dutsmiller" {
+  team_id  = github_team.admin.id
+  username = "dutsmiller"
+  role     = "member"
+}
+
+resource "github_team_repository" "some_team_repo" {
+  for_each = var.azure_workspaces
+
+  team_id    = github_team.admin.id
+  repository = github_repository.workspaces[each.key].name
+  permission = "admin"
+}
