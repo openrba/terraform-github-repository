253dbbd605afaed4e26bddcc2d31d1823480be2f
Joshua Hiscocks <joshua.hiscocks@lexisnexisrisk.com>
2021-01-28 13:01:38 +0000

    Additional subscriptions added to output

--- output.tf
+++ output.tf
@@ -3,11 +3,12 @@ output "azure_workspaces" {
   value = {
     for workspace in keys(var.repository_branches):
         workspace => {
-            tenant_id         = var.repository_branches[workspace].tenant_id
-            subscription_id   = var.repository_branches[workspace].subscription_id
-            cloud_name        = var.repository_branches[workspace].cloud_name
-            github_org        = var.github_org
-            github_identifier = github_repository.workspace.full_name
+            tenant_id                = var.repository_branches[workspace].tenant_id
+            subscription_id          = var.repository_branches[workspace].subscription_id
+            cloud_name               = var.repository_branches[workspace].cloud_name
+            github_org               = var.github_org
+            github_identifier        = github_repository.workspace.full_name
+            additional_subscriptions = var.repository_branches[workspace].azure_workspaces
         }
   }
 }
