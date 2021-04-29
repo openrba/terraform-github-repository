4bda380653c0f47d62cc74783d16e0aa37c0a7b4
Joshua Hiscocks <joshua.hiscocks@lexisnexisrisk.com>
2021-04-29 12:49:19 +0100

    Added support for GitHub Team name prefixes to prevent duplicates/overlap in TFE Prod/Dev

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # Create Repos
-resource "github_repository" "workspace" {  
+resource "github_repository" "workspace" {
   name         = "${var.repository_prefix}-${var.repository_name}"
   description  = var.repository_description
   visibility   = var.repository_visibility
   homepage_url = var.repository_homepage
 
   template {
-    owner = "LexisNexis-TFE"
+    owner      = "LexisNexis-TFE"
     repository = "tfe-workspace-template"
   }
 
@@ -14,25 +14,25 @@ resource "github_repository" "workspace" {
 
 # Create GitHub Teams
 resource "github_team" "workspace_read" {
-  name        = "ris-azr-group-github-${var.repository_name}-read"
+  name        = "${var.github_team_prefix}-${var.repository_name}-read"
   description = "${var.repository_name} Read Access"
   privacy     = "closed"
 }
 
 resource "github_team" "workspace_write" {
-  name        = "ris-azr-group-github-${var.repository_name}-write"
+  name        = "${var.github_team_prefix}-${var.repository_name}-write"
   description = "${var.repository_name} Write Access"
   privacy     = "closed"
 }
 
 resource "github_team" "approvers" {
-  name        = "ris-azr-group-github-${var.repository_name}-approvers"
+  name        = "${var.github_team_prefix}-${var.repository_name}-approvers"
   description = "${var.repository_name} Merge Request Approvers"
   privacy     = "closed"
 }
 
 # Team Permissions
-resource "github_team_repository" "team_read" { 
+resource "github_team_repository" "team_read" {
   team_id    = github_team.workspace_read.id
   repository = github_repository.workspace.name
   permission = "pull"
@@ -44,7 +44,7 @@ resource "github_team_repository" "team_write" {
   permission = "push"
 }
 
-resource "github_team_repository" "team_approvers" { 
+resource "github_team_repository" "team_approvers" {
   team_id    = github_team.approvers.id
   repository = github_repository.workspace.name
   permission = "push"
@@ -59,14 +59,14 @@ resource "github_branch" "branch" {
 }
 
 resource "github_branch_protection" "protection" {
-  for_each = toset([ for name, branch in var.repository_branches: name if branch.protected == "true" ])
+  for_each = toset([for name, branch in var.repository_branches : name if branch.protected == "true"])
 
   repository_id  = github_repository.workspace.node_id
   pattern        = each.key
   enforce_admins = true
 
   required_pull_request_reviews {
-    dismiss_stale_reviews = true
+    dismiss_stale_reviews           = true
     required_approving_review_count = 1
   }
 
@@ -82,7 +82,7 @@ resource "github_team_repository" "all_repos" {
 }
 
 # Global Team
-resource "github_team_repository" "global" { 
+resource "github_team_repository" "global" {
   count = var.repository_visibility == "internal" ? 1 : 0
 
   team_id    = var.github_global_team
--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "azure_workspaces" {
   description = "Subscription Map"
   value = {
-    for workspace in keys(var.repository_branches):
-        workspace => {
-            tenant_id                = var.repository_branches[workspace].tenant_id
-            subscription_id          = var.repository_branches[workspace].subscription_id
-            cloud_name               = var.repository_branches[workspace].cloud_name
-            github_org               = var.github_org
-            github_identifier        = github_repository.workspace.full_name
-            additional_subscriptions = var.repository_branches[workspace].additional_subscriptions
-        }
+    for workspace in keys(var.repository_branches) :
+    workspace => {
+      tenant_id                = var.repository_branches[workspace].tenant_id
+      subscription_id          = var.repository_branches[workspace].subscription_id
+      cloud_name               = var.repository_branches[workspace].cloud_name
+      github_org               = var.github_org
+      github_identifier        = github_repository.workspace.full_name
+      additional_subscriptions = var.repository_branches[workspace].additional_subscriptions
+    }
   }
 }
